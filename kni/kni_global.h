47a6105ae6b3767802341b7e6cb0c5441cfcf823
makis marimpis <makhsm@gmail.com>
2012-03-18 08:59:09 +0200

    Updated to KDE's macros.

--- kni/kni_global.h
+++ kni/kni_global.h
@@ -1,12 +1,16 @@
 #ifndef KNI_GLOBAL_H
 #define KNI_GLOBAL_H
 
-#include <QtCore/qglobal.h>
+#include <kdemacros.h>
 
 #if defined(KNI_LIBRARY)
-#  define KNISHARED_EXPORT Q_DECL_EXPORT
+#  define KNISHARED_EXPORT KDE_EXPORT
 #else
-#  define KNISHARED_EXPORT Q_DECL_IMPORT
+#  define KNISHARED_EXPORT KDE_IMPORT
 #endif
+
+# ifndef KNISHARED_EXPORT_DEPRECATED
+#  define KNISHARED_EXPORT_DEPRECATED KDE_DEPRECATED KNISHARED_EXPORT
+# endif
 
 #endif // KNI_GLOBAL_H
