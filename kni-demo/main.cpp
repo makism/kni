c61de16b6236c4719208326f0fbc69b8c052afbf
makis marimpis <makhsm@gmail.com>
2012-03-19 22:31:50 +0200

    * Using the new threaded interface.

--- kni-demo/main.cpp
+++ kni-demo/main.cpp
@@ -7,13 +7,9 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     KniOniContext context("/home/makism/Temp/ONIs/me.oni");
-//    KniDepthGenerator depth(&context);
     KniDepthGenerator& depth = context.depthGenerator();
 
-    for (int i=0; i<100; i++) {
-        context.update();
-        depth.update();
-    }
+    context.startAll();
 
-//    return a.exec();
+    return a.exec();
 }
